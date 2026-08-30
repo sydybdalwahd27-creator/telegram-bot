2a809387d86f7e8af8db1233e321cb820ce397c8
sydybdalwahd27-creator <sydybdalwahd27@gmail.com>
2026-08-30 21:42:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1288,32 +1288,25 @@ def main():
     
         application.add_handler(CallbackQueryHandler(button_callback))
     
-        application.add_handler(
-            MessageHandler(filters.TEXT & ~filters.COMMAND, handle_ai_chat)
-        )
 
         application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, handle_ai_chat)
-    )
+        )
 
-    application.add_handler(
+        application.add_handler(
         MessageHandler(
             filters.PHOTO | filters.Document.ALL | filters.VIDEO,
             handle_photo_document,
         )
-    )
+        )
 
-# معالج الأخطاء العام #
-application.add_error_handler(error_handler)
-
-print("Bot is running...")
-application.run_polling()
-
-# معالج الأخطاء العام #
+        # معالج الأخطاء العام #
         application.add_error_handler(error_handler)
 
-print("Bot is running...")
-    application.run_polling()
+        print("Bot is running...")
+        application.run_polling()
+
+
 
 
 if __name__ == "__main__":
