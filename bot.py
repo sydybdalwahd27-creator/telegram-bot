b66f1aec2d1e5863fc10c1f9a3c2327743f2a5ed
sydybdalwahd27-creator <sydybdalwahd27@gmail.com>
2026-08-30 20:24:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1290,22 +1290,23 @@ def main():
     
         application.add_handler(
             MessageHandler(filters.TEXT & ~filters.COMMAND, handle_ai_chat)
-        )
-        )
-        application.add_handler(
-            MessageHandler(
-                filters.PHOTO | filters.Document.ALL | filters.VIDEO,
-                handle_photo_document,
-            )
-        )
-    
-        # معالج الأخطاء العام
-        application.add_error_handler(error_handler)
-    
-        print("Bot is running...")
-        application.run_polling()
-    
-    
+     application.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, handle_ai_chat)
+)
+
+application.add_handler(
+    MessageHandler(
+        filters.PHOTO | filters.Document.ALL | filters.VIDEO,
+        handle_photo_document,
+    )
+)
+
+# معالج الأخطاء العام #
+application.add_error_handler(error_handler)
+
+print("Bot is running...")
+application.run_polling()
+
 
 if __name__ == "__main__":
-    main()
+  main()
